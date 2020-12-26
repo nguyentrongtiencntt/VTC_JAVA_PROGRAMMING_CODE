5fe2a6b8040d39605e0532ca8a2439d5b4c95c70
nguyentrongtiencntt <nguyentrongtiencntt@gmail.com>
2020-12-26 19:07:05 +0700

    Add somthing!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+
+
+        System.out.println("No thing to print!");
+
+
     }
 }
