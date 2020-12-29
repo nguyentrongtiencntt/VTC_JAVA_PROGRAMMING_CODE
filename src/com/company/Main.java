3c0426d7fab60c2f27e0b5c655ff359973c3e537
nguyentrongtiencntt <nguyentrongtiencntt@gmail.com>
2020-12-29 14:31:04 +0700

    Add somthing format string!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
-        System.out.println("No thing to print!");
+        String name = "N. T. Tien";
+        char gender = 'M';
+        int age = 25;
+
+        System.out.println("Name: "+name);
+        System.out.println("Gender: "+gender);
+        System.out.println("Age: "+age);
+
+        System.out.println("Name: "+ name + "\tGender: "+gender+ "\tAge: "+age);
+        //or use String.format()
+        System.out.println(String.format("Name: %s \tGender: %c\tAge: ", name,gender,age));
+
+
+
 
 
     }
