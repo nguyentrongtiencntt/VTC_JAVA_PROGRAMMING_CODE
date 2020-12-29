0d86056b0b7a0b7f8facb1596162a82c5473fc84
nguyentrongtiencntt <nguyentrongtiencntt@gmail.com>
2020-12-29 14:56:39 +0700

    Add sf1, sf2, sf3 class!

--- src/com/company/sf1.java
+++ src/com/company/sf1.java
@@ -1,16 +1,13 @@
 package com.company;
 
-import java.util.Scanner;
-
-public class Main {
-
+public class sf1 {
     public static void main(String[] args) {
 
 
         String name="sonoo";
         String sf1=String.format("name is %s",name);
         String sf2=String.format("value is %f",32.33434);
-        String sf3=String.format("value is %32.12f",32.33434);//returns 12 char fractional part filling with 0
+        String sf3=String.format("value is %20.12f",32.33434);//returns 12 char fractional part filling with 0
 
         System.out.println(sf1);
         System.out.println(sf2);
